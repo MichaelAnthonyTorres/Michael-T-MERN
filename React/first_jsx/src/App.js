bc78a3a7e7bf9ffa648f7d4ece71959b8903b8bc
Michael Torres <mtorres11289@yahoo.com>
2021-09-06 01:18:52 -0400

    First JSX

---
+++ React/first_jsx/src/App.js
@@ -0,0 +1,20 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header >
+        <h1>Hello Dojo!</h1>
+      </header>
+      <h3>Things I need to do:</h3>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mount Everest</li>
+        <li>Run a Marathon</li>
+        <li>Feed the dogs.</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
